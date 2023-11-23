393e064e4b4befe27274f67324e908308cbe4149
Dariel7 <81272117+Dariel7@users.noreply.github.com>
2023-11-23 19:19:14 -0400

    Add files via upload

---
+++ Cuarta clase/import.py
@@ -0,0 +1,19 @@
+#Importar Math
+import math
+x= 3.14
+y= math.sin(x)
+print(y)
+
+#Generar número Random
+import random
+
+numero= random.randint(1, 100)
+print("numero random:", numero)
+
+#Import numpy
+import numpy
+
+y= numpy.sin(x)
+print("numero numpy:", y)
+
+
